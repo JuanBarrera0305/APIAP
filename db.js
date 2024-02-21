2ff2c4506399e61333c841c7da111596afcfc810
JuanBarrera0305 <120711482+JuanBarrera0305@users.noreply.github.com>
2024-02-20 22:59:28 -0500

    Initial commit

---
+++ db.js
@@ -0,0 +1,20 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+
+    user: "jjbarre",
+    host: "dpg-cn5se1mv3ddc738mjhv0-a",
+    database: "library_leo8",
+    password: "t1UTZF8NIY12RTqfYQlx7DbU24xCUSMo",
+    database: "library",
+    port: 5432,
+
+    //user: "postgres",
+    //host: "localhost",
+    //database: "library",
+    //port:5432,
+
+
+});
+
+module.exports = pool;
