2f8766157a0785548d37f6ad34e39ef122776512
JuanBarrera0305 <120711482+JuanBarrera0305@users.noreply.github.com>
2024-03-12 08:49:11 -0400

    db update

--- db.js
+++ db.js
@@ -1,28 +1,15 @@
 const Pool = require("pg").Pool;
-
 const pool = new Pool({
-
-<<<<<<< HEAD
-    //user: "jjbarre",
-   // host: "dpg-cn5se1mv3ddc738mjhv0-a",
-   // database: "library_leo8",
-    //password: "t1UTZF8NIY12RTqfYQlx7DbU24xCUSMo",
-   // port: 5432,
-=======
     user: "jjbarre",
     host: "dpg-cn5se1mv3ddc738mjhv0-a",
     database: "library_leo8",
     password: "t1UTZF8NIY12RTqfYQlx7DbU24xCUSMo",
-    //database: "library",
-    port: 5432,
->>>>>>> origin/main
-
-    user: "postgres",
-    host: "localhost",
     database: "library",
-    port:5432,
+    port: 5432,
 
-
+    //user: "postgres",
+    //host: "localhost",
+    //database: "library",
+    //port:5432,
 });
-
 module.exports = pool;
